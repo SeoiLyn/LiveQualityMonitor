5b23da7515cfd5261620a572c7594c31ec5cc96e
WaterLin <liumangxuxu@126.com>
2017-03-07 11:53:03 +0800

    add monitor get method to refresh automatically

--- LiveQualityPanel/LiveQualityPanel.py
+++ LiveQualityPanel/LiveQualityPanel.py
@@ -1,9 +1,9 @@
 import web
 from subprocess import Popen
 from numpy import genfromtxt
+import uuid
 
 record_root = "/home/water/projects/LiveQualityPanel/"
-record_read = "static/data/receive-001.csv"
 
 urls = (
     '/', 'index',
@@ -17,11 +17,18 @@ class index:
         return render.index()
 
 class monitor:
-	def POST(self):
-		i = web.input()
-		flv_url = i['url']
-		p = Popen(['/home/water/projects/LiveQualityMonitor/debug/LiveQualityMonitor', flv_url, record_root+record_read])
-		return render.monitor(record_read)
+    def POST(self):
+        i = web.input()
+        flv_url = i['url']
+        name = str(uuid.uuid4())
+        record_read = "static/data/quality-"+ name +".csv"
+        p = Popen(['/home/water/projects/LiveQualityMonitor/debug/LiveQualityMonitor', flv_url, record_root+record_read])
+        return render.monitor(record_read)
+    def GET(self):
+        user_data = web.input()
+        #return "<h1>" + user_data.name + "</h1>"
+        print user_data.name
+        return render.monitor(user_data.name)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
