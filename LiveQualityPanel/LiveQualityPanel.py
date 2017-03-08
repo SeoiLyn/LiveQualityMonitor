16b6dab1c66c70c0b128c732638df90d5de49ee2
WaterLin <liumangxuxu@126.com>
2017-03-08 17:25:21 +0800

    config path and port according to config.cfg

--- LiveQualityPanel/LiveQualityPanel.py
+++ LiveQualityPanel/LiveQualityPanel.py
@@ -3,10 +3,14 @@ from subprocess import Popen
 from numpy import genfromtxt
 import uuid
 from threading import Timer
+import ConfigParser, os
 
-projects_root = "/home/water/projects/"
-record_root = projects_root+"LiveQualityPanel/"
-brisque_revised = projects_root + "LiveQualityMonitor/brisque_revised/"
+#define some default path and exe value
+projects_root = "/home/username/projects/"
+panel_root = projects_root+"LiveQualityPanel/"
+brisque_path = projects_root + "LiveQualityMonitor/brisque_revised/"
+monitor_exe = projects_root+'LiveQualityMonitor/debug/LiveQualityMonitor'
+sever_port = 8080
 
 urls = (
     '/', 'index',
@@ -14,6 +18,30 @@ urls = (
 )
 
 render = web.template.render('templates/')
+
+def readConfig():
+    config = ConfigParser.ConfigParser()
+    config.read('./config.cfg')
+
+    global projects_root
+    projects_root = config.get('path', 'projects_root')
+    print "config projects root: "+projects_root
+
+    global panel_root
+    panel_root = config.get('path', 'panel_root')
+    print "config panel root: "+panel_root
+
+    global monitor_exe
+    monitor_exe = config.get('path', 'monitor_exe')
+    print "config monitor exe: "+monitor_exe
+
+    global brisque_path
+    brisque_path = config.get('path', 'brisque_path')
+    print "config brisque path: "+brisque_path
+
+    global sever_port
+    sever_port = config.getint('server', 'port')
+    print "config server port: "+str(sever_port)
 
 def killMonitorProcess(*args):
     print "kill monitor process"
@@ -27,11 +55,17 @@ class index:
 
 class monitor:
     def POST(self):
+        readConfig()
+
         i = web.input()
         flv_url = i['url']
         name = str(uuid.uuid4())
         record_read = "static/data/quality-"+ name +".csv"
-        p = Popen([projects_root+'LiveQualityMonitor/debug/LiveQualityMonitor', flv_url, record_root+record_read, brisque_revised])
+
+        print panel_root
+        print monitor_exe
+        print brisque_path
+        p = Popen([monitor_exe, flv_url, panel_root+record_read,brisque_path])
 
         t = Timer(600.0, killMonitorProcess, [p])
         t.start() # after 300 seconds, timer will trigger to stop the subprocess
@@ -42,6 +76,13 @@ class monitor:
         print user_data.name
         return render.monitor(user_data.name)
 
+class MyApplication(web.application):
+    def run(self, port=8080, *middleware):
+        func = self.wsgifunc(*middleware)
+        return web.httpserver.runsimple(func, ('0.0.0.0', port))
+
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+    readConfig()
+
+    app = MyApplication(urls, globals())
+    app.run(port=sever_port)
